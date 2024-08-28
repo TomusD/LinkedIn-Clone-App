0452c1818d12f084b10627c9d2474957e4585a9c
LoukasPap <loukas.ppd@gmail.com>
2024-08-28 17:44:50 +0300

    Add TSL configuration to frontend

---
+++ frontend/app/src/main/java/com/example/front/HttpClient.java
@@ -0,0 +1,66 @@
+package com.example.front;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+
+public class HttpClient {
+    public static OkHttpClient getRetrofitInstance(InputStream cert, String ip) {
+        try {
+            // Load CAs from an InputStream
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+            // Load the certificate from res/raw
+//            InputStream caInput = context.getResources().openRawResource(R.raw.certificate);
+            Certificate ca;
+            try {
+                ca = cf.generateCertificate(cert);
+            } finally {
+                cert.close();
+            }
+
+            // Create a KeyStore containing the trusted CAs
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+            // Create a TrustManager that trusts the CAs in the KeyStore
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+
+            // Hostname verifier to avoid the error.
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) tmf.getTrustManagers()[0])
+                    .hostnameVerifier((hostname, session) -> hostname.equals(ip))
+                    .build();
+
+            return client;
+
+        } catch (CertificateException | IOException | KeyStoreException | NoSuchAlgorithmException |
+                 KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    }
--- frontend/app/src/main/java/com/example/front/data/ApiClient.kt
+++ frontend/app/src/main/java/com/example/front/data/ApiClient.kt
@@ -1,19 +1,68 @@
 package com.example.front.data
 
+import androidx.compose.ui.res.stringResource
 import com.example.front.BuildConfig
+import com.example.front.HttpClient
+import com.example.front.R
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
+import android.content.Context
+
+//val inputStream = context.applicationContext.resources
 object RetrofitClient {
     private const val IP = BuildConfig.ip
     private const val PORT = BuildConfig.port
 
-    private const val BASE_URL = "http://$IP:$PORT"
+    val cert: String = "-----BEGIN CERTIFICATE-----\n" +
+            "MIIFazCCA1OgAwIBAgIUQO4kpbgcZ8sELedUZ6k5K6ZVC9IwDQYJKoZIhvcNAQEL\n" +
+            "BQAwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoM\n" +
+            "GEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0yNDA4MjgxMjU1MzBaFw0yNTA4\n" +
+            "MjgxMjU1MzBaMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEw\n" +
+            "HwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwggIiMA0GCSqGSIb3DQEB\n" +
+            "AQUAA4ICDwAwggIKAoICAQCT8a2+QPgHjR3A739UgpJpizFp8HMZEk8zQ9u3Nsc0\n" +
+            "rhY6Y+h/cH4Ktzo6eSC7L2JA0KTnKnvh89Ce9ngvQsAzDopJXs1c+nN+KPgxykNw\n" +
+            "hOvTKaGALFFxDvKdXnt1klLxBDeygh+SGPA5gCTVlRFRxFmllLgmCmyT6bFdXNUx\n" +
+            "nrNQxMeKlMG2jhKsq1OTyqtzBN+x5dIWjBCqRYEc8IUt1X87MP0HjjVi6J2Bu2Ql\n" +
+            "Q0mZVHJLdG9jbmtAU8buX6OoTeoP+yUIJex3qrvHYpBdNcNl1c0f1wbgSQJ5kiHE\n" +
+            "Yt0vQxz2SyDwwXPwlGdUdqZ5cgW6epb9Hpe70ttFf1BQWCjc4RVu1oc+3xJwprsZ\n" +
+            "4b/tweXEAzasSk6tGdWNXj2k3MXe9dUZbFjlT3lI5Z0c60NdR19TM+YKOnccEUue\n" +
+            "50RCryBA53JgyXnaXBkzRjMNHcZbLtfk8yow04vRuSh0GABIoxcDlOO7FHwD7h0V\n" +
+            "WUGL4Csuun860wR8NRLnssFWSC0ML+IXkMd7wvY/CiSTAgJ+deVeZIZ77RHQbBSK\n" +
+            "05Z6VpJONoEz7mZ3xsx9OtxdIRcvcKRXJwtWccR0Lu/H/oLVHoizKfFvm4dERROI\n" +
+            "hKnalf0SiXoA/0PbpjOtrFL1E8lWSc9Qtn8w9rJwS9hVLhTzXXtXQZVqfoF0cpRJ\n" +
+            "+QIDAQABo1MwUTAdBgNVHQ4EFgQUOm+wEudUgA8j5ck4c0Sl1nSF9kQwHwYDVR0j\n" +
+            "BBgwFoAUOm+wEudUgA8j5ck4c0Sl1nSF9kQwDwYDVR0TAQH/BAUwAwEB/zANBgkq\n" +
+            "hkiG9w0BAQsFAAOCAgEAYz5E1qxvQBbhADBS1hU/dhzUC6OiP4u9dzzLairjYjk4\n" +
+            "nzFtbe/8yiTf0LHGut4nxtIdzB/oQAmlRDVzwbZmS4FKLWmJQkllpiBrgvoaunR3\n" +
+            "6ktRrInF8I0MGy4c5v8wSk53L+7cEoSV+iEDSKfKFb24KSQt7RezmWSmVLcUJASL\n" +
+            "iUn2PdLIh2Z5Qf4/uVBqnBHmVN0COQCT5w3NlYVW9QHpiznTz2n8EFv/a50gmp8n\n" +
+            "goTBzZOxVCNcHURdTF7vWopi3JROYXglSCENwF5jr1sOP6a9k30tq1ZY6Hepqzxh\n" +
+            "oMwUhmWkKk3pxOsEW+hh2IQAEg8bZX9oR3XpUV854iPSPshQcM2EeMpI7sOwU45A\n" +
+            "E/qbLro9L0oMfJC8Vm0snL0j+lfb8YkHfzPB9S6p1WVQ/aLMomKhPRe4TmwSHoiP\n" +
+            "IUhtVRwPsd4HNFLKvkdT25uADGeWbXc8o9+pprrFFLAe7Dc3deioEoV5q1pVn3HY\n" +
+            "ZWaSuk0sfwitERnNtXLX0MqQpqztkKA8tPidpHf/5KHZY/PI66akUgeL6v/XhRG+\n" +
+            "G3eZmo74qlTSO2lHWy8FZshaB32YdWn1na+rnhTFh470NFqj7vEfMi6VWoD5oBLi\n" +
+            "m5NS8qsj1fq7E3qetgIPND7HA7A6t7pgaWnTDE0q4bjVp0S4oHGc/N3r2ytRHqU=\n" +
+            "-----END CERTIFICATE-----\n"
+
+//    private const val BASE_URL = "domaintesturlwow.com"
+    private const val BASE_URL = "https://$IP:$PORT"
+
+    val inputStream = ByteArrayInputStream(cert.toByteArray())
+//    val inp : InputStream? = this.javaClass.getResourceAsStream("/raw/certificate.pem")
+
+    val client : OkHttpClient = HttpClient.getRetrofitInstance(inputStream, IP)
+
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 }
