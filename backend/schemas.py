eed7ebcdf1f33ea231e0a1f68270c51e2629e9d8
LoukasPap <loukas.ppd@gmail.com>
2024-08-30 03:33:28 +0300

    Create and update some backend schemas for JWT

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 """
 schemas.py
@@ -6,14 +6,35 @@ schemas.py
 These schemas are used for validating the schema of the classes
 """
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+class LoginUser(BaseModel):
+    email: EmailStr
+    password: str
+
+    # class Config:
+    #     orm_mode = True
+    #     use_enum_values = True
+
 class UserBase(BaseModel):
     name: str
     surname: str
-    email: str
-    password: str
+    email: EmailStr
     image_path: str
 
+class UserRegister(UserBase):
+    password: str
 
 # inherits
 class User(UserBase):
     id: int
+
+class UserLoginResponse(Token):
+    id: str
+
