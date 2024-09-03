320a3db01962f46441e95656f88aa29ea646a8cc
LoukasPap <loukas.ppd@gmail.com>
2024-09-03 19:45:31 +0300

    Add storing of JWT and fix login problem

--- frontend/app/src/main/java/com/example/front/data/response/Token.kt
+++ frontend/app/src/main/java/com/example/front/data/response/Token.kt
@@ -3,6 +3,6 @@ package com.example.front.data.response
 import com.google.gson.annotations.SerializedName
 
 data class Token(
-    @SerializedName("access_token") val accessToken: String?,
-    @SerializedName("token_type") val tokenType: String?
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("token_type") val tokenType: String
 )
