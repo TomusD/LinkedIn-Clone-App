ec4aedb7cccca4c0494c3f098ac3e67f8f0ef528
LoukasPap <loukas.ppd@gmail.com>
2024-09-18 04:01:45 +0300

    Update FriendsViewModel

--- frontend/app/src/main/java/com/example/front/activity/FriendsViewModel.kt
+++ frontend/app/src/main/java/com/example/front/activity/FriendsViewModel.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.front.data.ApiClient
+import com.example.front.data.base.ChatPreview
 import com.example.front.data.base.User
+import com.example.front.data.base.UserMessage
 import com.example.front.data.response.APIResponse
+import com.example.front.data.response.ChatsList
 import com.example.front.data.response.EducationList
+import com.example.front.data.response.MessagesList
 import com.example.front.data.response.SkillsList
 import com.example.front.data.response.UserInfo
 import com.example.front.data.response.WorkList
@@ -40,12 +44,56 @@ class FriendsViewModel: ViewModel() {
         })
     }
 
+    private val _chats = MutableStateFlow<List<ChatPreview>>(emptyList())
+    val chats: StateFlow<List<ChatPreview>> get() = _chats
+
+    fun getChats(context: Context) {
+        val apiClient = ApiClient()
+        val call = apiClient.getApiService(context).getChats()
+        call.enqueue(object : Callback<ChatsList> {
+            override fun onResponse(call: Call<ChatsList>, response: Response<ChatsList>) {
+                if (response.isSuccessful) {
+                    _chats.value = response.body()?.chatsPreviews ?: emptyList()
+                    Log.d("MYTEST", "CHATS-SUCCESS ${_chats.value}")
+                }
+            }
+
+            override fun onFailure(call: Call<ChatsList>, t: Throwable) {
+                Log.e("MYTEST", "CHATS-FAILURE: "+ t.message.toString())
+            }
+        })
+    }
+
+    private val _userMsg = MutableStateFlow<List<UserMessage>?>(null)
+    val userMsg: StateFlow<List<UserMessage>?> get() = _userMsg
+
+    fun getUserMessages(context: Context, userId: Int) {
+        val apiClient = ApiClient()
+        val call = apiClient.getApiService(context).getMessages(userId)
+        call.enqueue(object : Callback<MessagesList> {
+            override fun onResponse(call: Call<MessagesList>, response: Response<MessagesList>) {
+                if (response.isSuccessful) {
+                    _userMsg.value = response.body()?.messages
+                    Log.d("MYTEST", "FRIEND - SUCCESS === ${_userMsg.value}")
+                } else {
+                    Log.d("MYTEST", "FRIEND - NO SUCCESS === ${response.body()}")
+
+                }
+            }
+
+            override fun onFailure(call: Call<MessagesList>, t: Throwable) {
+                Log.e("MYTEST", "FRIEND - FAILURE: "+ t.message.toString())
+            }
+        })
+    }
+
 
     private val _userInfo = MutableStateFlow<UserInfo?>(
         UserInfo(WorkList(emptyList()), EducationList(emptyList()),
             SkillsList(emptyList()), false
         )
     )
+
     val userInfo: StateFlow<UserInfo?> get() = _userInfo
 
     fun getPublicInfo(context: Context, userId: Int) {
